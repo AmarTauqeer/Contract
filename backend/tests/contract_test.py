f89377cff958d5c7643f2de0908eeed3c06b1832
amar <amar.tauqeer@gmail.com>
2021-10-01 12:55:54 +0200

    contract path changed

--- backend/tests/contract_test.py
+++ backend/tests/contract_test.py
@@ -4,7 +4,7 @@ import requests
 
 class ContractApiTest(unittest.TestCase):
     # base url
-    CONTRACT_URL = "http://172.25.5.173:5000/contract/"
+    CONTRACT_URL = "https://actool.contract.sti2.at/contract/"
     DATA = {
         "Amendment": "abccc",
         "ConfidentialityObligation": "string",
@@ -38,8 +38,8 @@ class ContractApiTest(unittest.TestCase):
         r = requests.get(ContractApiTest.CONTRACT_URL + "list_of_contracts/")
         self.assertEqual(r.status_code, 200)
 
-
     # get all contracts
+
     def test_get_all_contractors(self):
         r = requests.get(ContractApiTest.CONTRACT_URL + "contractors/")
         self.assertEqual(r.status_code, 200)
@@ -47,33 +47,39 @@ class ContractApiTest(unittest.TestCase):
     # get contract by requester
     def test_get_contract_by_requester(self):
         requester = 'CompanyABC'
-        r = requests.get(ContractApiTest.CONTRACT_URL + "by_requester/{}/".format(requester))
+        r = requests.get(ContractApiTest.CONTRACT_URL +
+                         "by_requester/{}/".format(requester))
         self.assertEqual(r.status_code, 200)
 
     # get contract by provider
     def test_get_contract_by_provider(self):
         provider = 'Brade'
-        r = requests.get(ContractApiTest.CONTRACT_URL + "by_provider/{}/".format(provider))
+        r = requests.get(ContractApiTest.CONTRACT_URL +
+                         "by_provider/{}/".format(provider))
         self.assertEqual(r.status_code, 200)
 
     # get contract by contract id
     def test_get_contract_by_id(self):
         id = 'kg244565'
-        r = requests.get(ContractApiTest.CONTRACT_URL + "by_contractId/{}/".format(id))
+        r = requests.get(ContractApiTest.CONTRACT_URL +
+                         "by_contractId/{}/".format(id))
         self.assertEqual(r.status_code, 200)
 
     # new contract
     def test_new_contract(self):
-        r = requests.post(ContractApiTest.CONTRACT_URL + "create/", json=ContractApiTest.DATA)
+        r = requests.post(ContractApiTest.CONTRACT_URL +
+                          "create/", json=ContractApiTest.DATA)
         self.assertEqual(r.status_code, 200)
 
     # update contract
     def test_update_contract(self):
-        r = requests.put(ContractApiTest.CONTRACT_URL + "update/", json=ContractApiTest.DATA)
+        r = requests.put(ContractApiTest.CONTRACT_URL +
+                         "update/", json=ContractApiTest.DATA)
         self.assertEqual(r.status_code, 200)
 
     # delete contract by contract id
     def test_get_contract_by_id(self):
         id = 'kgtest'
-        r = requests.delete(ContractApiTest.CONTRACT_URL + "delete/{}/".format(id))
+        r = requests.delete(ContractApiTest.CONTRACT_URL +
+                            "delete/{}/".format(id))
         self.assertEqual(r.status_code, 200)
